3a4fb10e85e93895c4de05773bd7a1ac6d292aad
fghebremariam <fghebremariam@FGHEBREM-T470.na.nice.com>
2018-05-01 22:55:52 -0500

    initial commit

---
+++ oauth-microservice/src/main/java/com/nice/oauth/ServiceConfig.java
@@ -0,0 +1,24 @@
+package com.nice.oauth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+
+@Configuration
+public class ServiceConfig extends GlobalAuthenticationConfigurerAdapter {
+	
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception 
+	{
+		auth
+		    .inMemoryAuthentication()
+		        .withUser("user1")
+		        .password("pass1")
+		        .roles("ADD_BOOKING", "VIEW_BOOKING")
+	        .and()
+	            .withUser("user2")
+	            .password("pass2")
+	            .roles("VIEW_BOOKING");
+	}
+
+}
